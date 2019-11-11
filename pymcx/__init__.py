41a4ed880c0f545fce714962e94a2c558b2487bc
MBaillot <maxime.baillot.1@ulaval.ca>
2019-11-11 10:03:41 -0500

    Change Python package name from mcx to pymcx (because of mcx bin)

--- pymcx/__init__.py
+++ pymcx/__init__.py
@@ -1,7 +1,7 @@
 from .load_mc2 import loadmc2
 from .load_mch import loadmch
 from .mcx_create import create
-
+from .mcx_run import run
 
 __version__ = '1.0'
 __all__ = ['loadmc2', 'loadmch', 'create']
