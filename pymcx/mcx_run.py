e7447ab9d2316e4c332a1130a6e3c771f403cd0c
MBaillot <maxime.baillot.1@ulaval.ca>
2019-11-11 11:06:31 -0500

    working mcxrun

--- pymcx/mcx_run.py
+++ pymcx/mcx_run.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-def run(cfg,flag,mcxbin = './mcx'):
+def run(cfg, flag, mcxbin = './mcx'):
 
 	import numpy as np
 	import os, json
 	
 	SID = cfg['Session']['ID']
+
 	f = open(SID+'.json', 'w')
 	f.write(json.dumps(cfg, sort_keys=True, indent=2))
 	f.close()
-	
-	
+
 	os.system(mcxbin+' -f '+SID+'.json '+flag)
