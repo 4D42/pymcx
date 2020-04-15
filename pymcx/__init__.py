bedf871ef63e759ba5b8c9cb9cd0e9b902bdf4af
MBaillot <maxime.baillot.1@ulaval.ca>
2020-04-15 11:46:26 -0400

    adding photon detweight function in init

--- pymcx/__init__.py
+++ pymcx/__init__.py
@@ -2,7 +2,8 @@ from .load_mc2 import loadmc2
 from .load_mch import loadmch
 from .mcx_create import create
 from .mcx_run import run
-from .mcx_plotvol import mcxplotvol
+#from .mcx_plotvol import mcxplotvol
+from .mcx_detweight import detweight
 
 __version__ = '1.0'
-__all__ = ['loadmc2', 'loadmch', 'create']
+__all__ = ['loadmc2', 'loadmch', 'create','run','detweight']
